5fb14b14f1ad28c10d125c2960b0dc44e69bf94f
Anand Vidvat <anandvidvat@gmail.com>
2017-07-07 01:58:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+﻿const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+
+let win;
+
+
+function CreateWindow() {
+    //create Browser window
+    win = new BrowserWindow({
+        width: 800, heigth:600
+    })
+    //load index.hmtl
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    win.on('closed', () => { win = null; });
+
+}
+
+app.on('ready', CreateWindow);
